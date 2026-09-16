a1c9863ccfee81a953423d28ab6ecb70e86abdb3
SamiMansuri <samimansuri303@gmail.com>
2026-09-16 23:39:50 +0530

    fix build issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
